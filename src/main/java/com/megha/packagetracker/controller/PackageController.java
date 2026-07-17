de167df5c6ddf9b8fecb52bd3ad640dc3d07de51
Meghagasti27 <meghagasti969@gmail.com>
2026-07-17 23:01:06 +0530

    Added service and package lookup endpoint

--- src/main/java/com/megha/packagetracker/controller/PackageController.java
+++ src/main/java/com/megha/packagetracker/controller/PackageController.java
@@ -1,8 +1,9 @@
 package com.megha.packagetracker.controller;
-
 import com.megha.packagetracker.entity.PackageEntity;
 import com.megha.packagetracker.service.PackageService;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,8 +14,13 @@ public class PackageController {
 	public PackageController(PackageService packageService) {
 		this.packageService=packageService;
 	}
+	@GetMapping("/packages")
+	public List<PackageEntity> getPackages() {
+	    return packageService.getAllPackages();
+	}
+
 	@GetMapping("/packages/{id}")
 	public PackageEntity getPackageById(@PathVariable Long id) {
-		return packageService.getPackageById(id);
+	    return packageService.getPackageById(id);
 	}
 }
--- src/main/java/com/megha/packagetracker/service/PackageService.java
+++ src/main/java/com/megha/packagetracker/service/PackageService.java
@@ -2,30 +2,34 @@ package com.megha.packagetracker.service;
 
 import com.megha.packagetracker.entity.PackageEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import java.util.List;
-@Service
 
+import java.util.List;
+
+@Service
 public class PackageService {
-	public List<PackageEntity> getAllPackages(){
-		return List.of(
-				new PackageEntity(
+
+    public List<PackageEntity> getAllPackages() {
+        return List.of(
+                new PackageEntity(
                         1L,
                         "DTDC123",
                         "DTDC",
                         "Amazon",
                         "Megha",
                         "In Transit"
-                ),
-                new PackageEntity(
-                        2L,
-                        "BD456",
-                        "Blue Dart",
-                        "Flipkart",
-                        "Megha",
-                        "Delivered"
                 )
-                );
-	}
-	
+        );
+    }
+
+    public PackageEntity getPackageById(Long id) {
+
+        return new PackageEntity(
+                id,
+                "DTDC123",
+                "DTDC",
+                "Amazon",
+                "Megha",
+                "In Transit"
+        );
+    }
 }
